62b627f423d1419bab7bbfaae1b41b987e9d0187
Josie Gioffre Dean <josie.gioffredean@nab.com.au>
2018-06-20 09:40:50 +1000

    added template, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # python-stuff
 
 Expanding on the contents in here: http://flask.pocoo.org/
+
+
+## Starting Flask
+To start, execute `./run.sh playing_with_flask.sh`.  This will hog the terminal so Ctrl-C will kill the process.
+This can be accessed in the browser at `http://localhost:5000`
+
+## Routes/Endpoints
+- `/`: default route, returns a page saying `Hello World!`
+- `/text`: renders the questions as plain text on the page, with no input capability
+- `fancy`: renders the questions using the `template/questions.html` to render quetsions with formatting and inputs.
--- playing_with_flask.py
+++ playing_with_flask.py
@@ -1,20 +1,29 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-questions = [
-  {"id": "1", "text": "This is question 1", "options": ["option 1.1", "option 1.2", "option 1.3"] },
-  {"id": "2", "text": "This is question 2", "options": ["option 2.1", "option 2.2", "option 2.3"] }
-]
-
-@app.route("/questions")
+# This method returns questions. Note it is called in both methods executed by the routes.
+# going forward, you could update this method to read questions from CSV or Dynamo.
 def get_questions():
+    questions = [
+      {"id": "1", "text": "This is question 1", "options": ["option 1.1", "option 1.2", "option 1.3"], "type": "radio" },
+      {"id": "2", "text": "This is question 2", "options": ["option 2.1", "option 2.2", "option 2.3"], "type": "checkbox" }
+    ]
+    return questions
+
+@app.route("/text")
+def get_questions_as_text():
     all_questions = ''
-    for question in questions:
+    for question in get_questions():
         for k, v in question.items():
             all_questions = all_questions + ''.join('{}: {}'.format(k, v)) + '</br>'
 
     return all_questions
+
+@app.route('/fancy')
+def get_questions_rendered():
+    return render_template('questions.html', questions=get_questions())
